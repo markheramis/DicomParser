fa33f9b9ece67ecc46bbee2572160c5f585fe5db
Mark Heramis <chumheramis@gmail.com>
2023-07-11 04:05:43 +0800

    return a C-Compatible null terminated string for external use ability
    Signed-off-by: Mark Heramis <chumheramis@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -7,16 +7,34 @@ use dicom_object::open_file;
 use dicom_object::Tag;
 use serde::Serialize;
 use serde_json::json;
-
 use serde_json::Value as JsonValue;
 use std::collections::HashMap;
+use std::ffi::CStr;
+use std::ffi::CString;
+use std::os::raw::c_char;
+use std::ptr;
+use std::fs;
 use std::path::Path;
+
 
 #[derive(Serialize)]
 struct DicomProps(HashMap<String, Option<String>>);
 
 #[no_mangle]
-pub fn parse(path: &Path, show_tag: bool) -> String {
+pub extern "C" fn parse(path: *const c_char, show_tag: bool) -> *mut c_char {
+    let c_str = unsafe { CStr::from_ptr(path) };
+    let path_str = match c_str.to_str() {
+        Err(_) => return ptr::null_mut(),
+        Ok(string) => string,
+    };
+    let path = Path::new(&path_str);
+    if !path.exists() {
+        panic!("File does not exist");
+    }
+    if !path.is_file() {
+        panic!("Path does not point to a file");
+    }
+    let path = fs::canonicalize(path).expect("Failed to resolve path");
     let allowed_vrs: Vec<VR> = vec![
         // Strings
         VR::AE,
@@ -72,7 +90,9 @@ pub fn parse(path: &Path, show_tag: bool) -> String {
             .collect(),
     );
     let json: JsonValue = json!(dicom_props);
-    return json.to_string();
+    let json_str = json.to_string();
+    let c_string = CString::new(json_str).unwrap();
+    c_string.into_raw()
 }
 
 fn get_tag_name(tag: Tag) -> String {
@@ -135,31 +155,50 @@ fn get_value(value: &DicomValue<dicom_object::InMemDicomObject, Vec<u8>>) -> Opt
     }
 }
 
+#[no_mangle]
+pub extern "C" fn free_string(s: *mut c_char) {
+    unsafe {
+        if s.is_null() { return }
+        CString::from_raw(s)
+    };
+}
 #[cfg(test)]
 
 mod tests {
+
+    use std::ffi::{CStr, CString};
     use serde_json::Error as JsonError;
     use super::*;
+
     fn parse_json(json_string: &str) -> Result<JsonValue, JsonError> {
         let parsed: JsonValue = serde_json::from_str(json_string)?;
         Ok(parsed)
     }
+
     #[test]
     fn test_parse_no_tag_should_not_be_null_with_ext() {
-        let path: &Path = Path::new("./assets/dicom-with-ext.dcm");
-        let result: String = parse(path, false);
+        let path = CString::new("./assets/dicom-with-ext.dcm").unwrap();
+        let result_ptr = parse(path.as_ptr(), false);
+        let result = unsafe { CStr::from_ptr(result_ptr).to_string_lossy().into_owned() };
+        free_string(result_ptr);
         assert!(!result.is_empty());
     }
+
     #[test]
     fn test_parse_with_tag_should_not_be_null_with_ext() {
-        let path: &Path = Path::new("./assets/dicom-with-ext.dcm");
-        let result: String = parse(path, true);
+        let path = CString::new("./assets/dicom-with-ext.dcm").unwrap();
+        let result_ptr = parse(path.as_ptr(), true);
+        let result = unsafe { CStr::from_ptr(result_ptr).to_string_lossy().into_owned() };
+        free_string(result_ptr);
         assert!(!result.is_empty());
     }
+
     #[test]
     fn test_parse_with_tag_should_have_correct_study_instance_uid_with_ext() {
-        let path: &Path = Path::new("./assets/dicom-with-ext.dcm");
-        let result: String = parse(path, true);
+        let path = CString::new("./assets/dicom-with-ext.dcm").unwrap();
+        let result_ptr = parse(path.as_ptr(), true);
+        let result = unsafe { CStr::from_ptr(result_ptr).to_string_lossy().into_owned() };
+        free_string(result_ptr);
         match parse_json(result.as_str()) {
             Ok(parsed_json) => {
                 if let Some(parsed_value) = parsed_json["StudyInstanceUID"].as_str() {
@@ -174,10 +213,13 @@ mod tests {
             Err(e) => println!("Could not parse JSON: {}", e),
         }
     }
+
     #[test]
     fn test_parse_no_tag_should_have_correct_study_instance_uid_with_ext() {
-        let path: &Path = Path::new("./assets/dicom-with-ext.dcm");
-        let result: String = parse(path, false);
+        let path = CString::new("./assets/dicom-with-ext.dcm").unwrap();
+        let result_ptr = parse(path.as_ptr(), false);
+        let result = unsafe { CStr::from_ptr(result_ptr).to_string_lossy().into_owned() };
+        free_string(result_ptr);
         match parse_json(result.as_str()) {
             Ok(parsed_json) => {
                 if let Some(parsed_value) = parsed_json["(0020,000D)"].as_str() {
@@ -186,7 +228,7 @@ mod tests {
                     //let expected_value = "1.2.826.0.1.3680043.2.594.20385.9727.6138.15049.24610\0";
                     assert_eq!(parsed_value, expected_value);
                 } else {
-                    panic!("(0020,0052) is not found in the parsed json");
+                    panic!("(0020,000D) is not found in the parsed json");
                 }
             }
             Err(e) => println!("Could not parse JSON: {}", e),
@@ -194,16 +236,17 @@ mod tests {
     }
     #[test]
     fn test_parse_with_tag_should_have_correct_study_instance_uid_on_no_ext() {
-        let path: &Path = Path::new("./assets/dicom-no-ext");
-        let result: String = parse(path, true);
+        let path = CString::new("./assets/dicom-no-ext").unwrap();
+        let result_ptr = parse(path.as_ptr(), true);
+        let result = unsafe { CStr::from_ptr(result_ptr).to_string_lossy().into_owned() };
+        free_string(result_ptr);
         match parse_json(result.as_str()) {
             Ok(parsed_json) => {
                 if let Some(parsed_value) = parsed_json["StudyInstanceUID"].as_str() {
-                    let expected_value =
-                        "1.3.46.670589.11.3540642177.2867929537.1763690001.2563942908";
+                    let expected_value = "1.3.46.670589.11.3540642177.2867929537.1763690001.2563942908";
                     assert_eq!(parsed_value, expected_value);
                 } else {
-                    panic!("(0020,0052) is not found in the parsed json");
+                    panic!("StudyInstanceUID is not found in the parsed json");
                 }
             }
             Err(e) => println!("Could not parse JSON: {}", e),
@@ -211,16 +254,17 @@ mod tests {
     }
     #[test]
     fn test_parse_no_tag_should_have_correct_study_instance_uid_on_no_ext() {
-        let path: &Path = Path::new("./assets/dicom-no-ext");
-        let result: String = parse(path, false);
+        let path = CString::new("./assets/dicom-no-ext").unwrap();
+        let result_ptr = parse(path.as_ptr(), false);
+        let result = unsafe { CStr::from_ptr(result_ptr).to_string_lossy().into_owned() };
+        free_string(result_ptr);
         match parse_json(result.as_str()) {
             Ok(parsed_json) => {
                 if let Some(parsed_value) = parsed_json["(0020,000D)"].as_str() {
-                    let expected_value =
-                        "1.3.46.670589.11.3540642177.2867929537.1763690001.2563942908";
+                    let expected_value = "1.3.46.670589.11.3540642177.2867929537.1763690001.2563942908";
                     assert_eq!(parsed_value, expected_value);
                 } else {
-                    panic!("(0020,0052) is not found in the parsed json");
+                    panic!("(0020,000D) is not found in the parsed json");
                 }
             }
             Err(e) => println!("Could not parse JSON: {}", e),
